af9484f6650f03a1e9126b6fa085cd2e80ddc315
Abhay Kshatriya <kshatriya.abhay@iitg.ac.in>
2019-07-18 08:18:07 +0530

    violet: POSP is coming

--- potato_violet.mk
+++ potato_violet.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := mk_violet
+PRODUCT_NAME := potato_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
