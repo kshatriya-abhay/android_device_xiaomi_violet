2c4a173f7b4bd3f0ff477327dc2e3418f0bf3de5
Abhay Kshatriya <kshatriya.abhay@alumni.iitg.ac.in>
2021-10-29 19:48:24 +0530

    bringup arrow-os again
    Joker pro

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,13 @@
 #
 # Copyright (C) 2019 The LineageOS Project
-# Copyright (C) 2020 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_violet.mk
+    $(LOCAL_DIR)/arrow_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_violet-userdebug \
-    superior_violet-user \
-    superior_violet-eng
+    arrow_violet-userdebug \
+    arrow_violet-user \
+    arrow_violet-eng
--- arrow_violet.mk
+++ arrow_violet.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2019-2020 The LineageOS Project
-# Copyright (C) 2018-2020 The SuperiorOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,8 +7,8 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -24,7 +23,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_USES_FACE_UNLOCK := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_violet
+PRODUCT_NAME := arrow_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
